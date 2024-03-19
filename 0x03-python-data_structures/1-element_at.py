029e17215296f9216a6deed4ad2b3a13fe50f5fa
Lutendo <maphalagl@gmail.com>
2024-03-19 07:00:01 -0700

    python-data_structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+def element_at(my_list, idx):
+    if idx < 0:
+        return "None"
+    elif idx > len(my_list) -1:
+        return "None"
+    else:
+        return my_list[idx]
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
-    elif idx > len(my_list) - 1:
-        return (my_list)
-    else:
-        new_list= my_list[:]
+    length = len(my_list)
+
+    new_list = my_list[:]
+
+    if 0 <= idx < length:
         new_list[idx] = element
-        return (new_list)
+
+    return (new_list)
